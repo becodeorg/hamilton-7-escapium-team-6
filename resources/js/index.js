15050ea4e480d1ad9c529feaa34f89791c28c810
GeorisVal <georis.val@gmail.com>
2022-10-31 17:00:28 +0100

    Added Navbar import to index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,5 +3,11 @@ async function addFooter() {
     const html = await resp.text();
     document.body.insertAdjacentHTML("beforeend", html);
 }
+async function addNavbar() {
+    const resp = await fetch("../../navbar.html");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("beforebegin", html);
+}
 
 addFooter();
+addNavbar();
