710714f285c8cc19f8dd5ec22fda98594585c053
GeorisVal <georis.val@gmail.com>
2022-10-31 11:29:12 +0100

    Added script to link footer to homepage

---
+++ resources/js/index.js
@@ -0,0 +1,7 @@
+async function addFooter() {
+    const resp = await fetch("../../footer.html");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("beforeend", html);
+}
+
+addFooter();
