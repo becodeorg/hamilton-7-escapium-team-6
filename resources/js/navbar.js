6950947788301f4d05fc11942ead0066ab3b5136
GeorisVal <georis.val@gmail.com>
2022-11-03 15:41:48 +0100

    Temp navbar JS changes

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -24,3 +24,14 @@ function checkboxBurger() {
         document.querySelector('nav').style.backgroundColor = "rgba(25, 25, 25, 0.4)"
     }
     }
+
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector("nav").style.top = "0";
+    } else {
+        document.querySelector("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+    } 
