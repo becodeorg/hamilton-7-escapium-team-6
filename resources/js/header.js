3b37e1116a2f18d92afbb847bdb09c7907474155
GeorisVal <georis.val@gmail.com>
2022-11-03 14:42:23 +0100

    Working on header

--- resources/js/header.js
+++
@@ -1,6 +0,0 @@
-let url = window.location.pathname;
-let filename = url.substring(url.lastIndexOf('/')+1);
-let titleMain = document.getElementById('header--title');
-filename = filename.split(".");
-console.log(filename[0]);
-titleMain.innerHTML = filename[0];
