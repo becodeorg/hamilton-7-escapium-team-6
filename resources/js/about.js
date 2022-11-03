f285ae24cf47437bc5234d0a7b20af86a82f568f
GeorisVal <georis.val@gmail.com>
2022-11-03 14:42:41 +0100

    Addition of navbar, header & footer to about page

--- resources/js/about.js
+++ resources/js/about.js
@@ -8,6 +8,18 @@ async function addNavbar() {
     const html = await resp.text();
     document.body.insertAdjacentHTML("beforebegin", html);
 }
+async function addHeader() {
+    const resp = await fetch("../../header.html");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("beforebegin", html);
+}
 
 addFooter();
+addHeader();
 addNavbar();
+
+/* let url = window.location.pathname;
+let filename = url.substring(url.lastIndexOf('/')+1);
+filename = filename.split(".");
+console.log(filename[0]);
+document.getElementById("header--title").innerHTML = filename[0]; */
