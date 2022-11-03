6ce3e6a14c87c52ce295bb103b4ad00a705354a8
GeorisVal <georis.val@gmail.com>
2022-11-03 10:13:27 +0100

    Added Navbar & Footer JS

---
+++ resources/js/about.js
@@ -0,0 +1,13 @@
+async function addFooter() {
+    const resp = await fetch("../../footer.html");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("beforeend", html);
+}
+async function addNavbar() {
+    const resp = await fetch("../../navbar.html");
+    const html = await resp.text();
+    document.body.insertAdjacentHTML("beforebegin", html);
+}
+
+addFooter();
+addNavbar();
